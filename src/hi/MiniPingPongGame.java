a5c5742d3d69b149617eed31f578f81a0c62d867
1ncursio <ckswn1323@naver.com>
2021-03-21 19:22:21 +0900

    [UPDATE] MiniPingPongGame

--- src/hi/MiniPingPongGame.java
+++ src/hi/MiniPingPongGame.java
@@ -7,46 +7,36 @@ import javax.swing.WindowConstants;
 import java.awt.*;
 import java.awt.event.*;
 
-public class MiniPingPongGame extends JPanel implements KeyListener {
+public class MiniPingPongGame extends JPanel implements Runnable {
     private static final long serialVersionUID = 1L;
 
     private Ball ball;
-    private Racquet racquet1;
-    private Racquet racquet2;
+    private GamePaddle paddle1;
+    private GamePaddle paddle2;
 
     public MiniPingPongGame() {
         ball = new Ball(this, Color.red);
-        this.setBackground(Color.green);
-        racquet1 = new Racquet(this, 10, 150, Color.blue);
-        racquet2 = new Racquet(this, 560, 150, Color.yellow);
+        this.setBackground(Color.black);
+        paddle1 = new GamePaddle(10, 150, 10, 80, 1);
+        paddle2 = new GamePaddle(560, 150, 10, 80, 2);
         this.setFocusable(true);
-        this.addKeyListener(this);
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public void keyPressed(KeyEvent e) {
-        // TODO Auto-generated method stub
-        racquet1.keyPressed(e);
-        racquet2.keyPressed(e);
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
-        // TODO Auto-generated method stub
-        racquet1.keyReleased(e);
-        racquet2.keyReleased(e);
     }
 
     void move() {
         ball.move();
-        racquet1.move();
-        racquet2.move();
+        paddle1.move();
+        paddle2.move();
+    }
+
+    void checkCollision() {
+        if (ball.x + ball.xSpeed < 0)
+            ball.xSpeed = 3;
+        if (ball.x + ball.xSpeed > this.getWidth() - 2 * Ball.RADIUS)
+            ball.xSpeed = -3;
+        if (ball.y + ball.ySpeed < 0)
+            ball.ySpeed = 3;
+        if (ball.y + ball.ySpeed > this.getHeight() - 2 * Ball.RADIUS)
+            ball.ySpeed = -3;
     }
 
     @Override
@@ -54,8 +44,24 @@ public class MiniPingPongGame extends JPanel implements KeyListener {
         super.paint(g);
         Graphics2D g2d = (Graphics2D) g;
         ball.draw(g2d);
-        racquet1.draw(g2d);
-        racquet2.draw(g2d);
+        paddle1.draw(g2d);
+        paddle2.draw(g2d);
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+        while (true) {
+            try {
+                Thread.sleep(10);
+                move();
+                checkCollision();
+                repaint();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -66,21 +72,21 @@ public class MiniPingPongGame extends JPanel implements KeyListener {
         MiniPingPongGame game = new MiniPingPongGame();
         frame.add(game);
         frame.setVisible(true);
-        while (true) {
-            game.move();
-            game.repaint();
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                // TODO: handle exception
-                e.printStackTrace();
-            }
-        }
+        // while (true) {
+        // game.move();
+        // game.repaint();
+        // try {
+        // Thread.sleep(10);
+        // } catch (InterruptedException e) {
+        // // TODO: handle exception
+        // e.printStackTrace();
+        // }
+        // }
     }
 
     class Ball {
         private static final int RADIUS = 20;
-        private int x = 0, y = 0, xSpeed = 1, ySpeed = 1;
+        private int x = 0, y = 0, xSpeed = 3, ySpeed = 3;
         private MiniPingPongGame game;
         private Color color;
 
@@ -91,20 +97,24 @@ public class MiniPingPongGame extends JPanel implements KeyListener {
 
         void move() {
             if (x + xSpeed < 0)
-                xSpeed = 1;
+                xSpeed = 3;
             if (x + xSpeed > game.getWidth() - 2 * RADIUS)
-                xSpeed = -1;
+                xSpeed = -3;
             if (y + ySpeed < 0)
-                ySpeed = 1;
+                ySpeed = 3;
             if (y + ySpeed > game.getHeight() - 2 * RADIUS)
-                ySpeed = -1;
+                ySpeed = -3;
+            if (collision()) {
+                xSpeed = -xSpeed;
+                System.out.println("collision");
+
+            }
             x += xSpeed;
             y += ySpeed;
         }
 
         private boolean collision() {
-            return game.racquet1.getBounds().intersects(getBounds())
-                    || game.racquet2.getBounds().intersects(getBounds());
+            return game.paddle1.getBounds().intersects(getBounds()) || game.paddle2.getBounds().intersects(getBounds());
         }
 
         public void draw(Graphics2D g) {
@@ -117,52 +127,9 @@ public class MiniPingPongGame extends JPanel implements KeyListener {
         }
     }
 
-    class Racquet {
-        private static final int WIDTH = 10;
-        private static final int HEIGHT = 80;
-        private int x = 0, y = 0;
-        private int xSpeed = 0;
-        private int ySpeed = 0;
-        private MiniPingPongGame game;
-        private Color color;
-
-        public Racquet(MiniPingPongGame game, int x, int y, Color color) {
-            this.game = game;
-            this.x = x;
-            this.y = y;
-            this.color = color;
-        }
-
-        public void move() {
-            if (y + ySpeed > 0 && y + ySpeed < game.getHeight() - HEIGHT)
-                y += ySpeed;
-        }
-
-        public void draw(Graphics2D g) {
-            g.setColor(color);
-            g.fillRect(x, y, WIDTH, HEIGHT);
-        }
-
-        public void keyReleased(KeyEvent e) {
-            ySpeed = 0;
-        }
-
-        public void keyPressed(KeyEvent e) {
-            if (e.getKeyCode() == KeyEvent.VK_UP)
-                ySpeed = -3;
-            else if (e.getKeyCode() == KeyEvent.VK_DOWN)
-                ySpeed = 3;
-
-        }
-
-        public Rectangle getBounds() {
-            return new Rectangle(x, y, WIDTH, HEIGHT);
-        }
-    }
-
     public class Score {
-        private static int GAME_WIDTH;
-        private static int GAME_HEIGHT;
+        private int GAME_WIDTH;
+        private int GAME_HEIGHT;
         protected int player1;
         protected int player2;
 
@@ -185,14 +152,19 @@ public class MiniPingPongGame extends JPanel implements KeyListener {
     class GamePaddle extends Rectangle implements KeyListener {
         private static final long serialVersionUID = 1L;
 
-        private int x = 0, y = 0;
-        private int xSpeed = 0;
+        private MiniPingPongGame game;
+        private int x, y, width, height;
         private int ySpeed = 0;
         private int id;
 
         GamePaddle(int x, int y, int width, int height, int id) {
             super(x, y, width, height);
+            this.x = x;
+            this.y = y;
             this.id = id;
+            this.width = width;
+            this.height = height;
+            addKeyListener(this);
         }
 
         @Override
@@ -203,6 +175,20 @@ public class MiniPingPongGame extends JPanel implements KeyListener {
 
         @Override
         public void keyPressed(KeyEvent e) {
+            if (this.id == 1) {
+                if (e.getKeyCode() == KeyEvent.VK_W)
+                    ySpeed = -3;
+                else if (e.getKeyCode() == KeyEvent.VK_S)
+                    ySpeed = 3;
+
+            } else if (this.id == 2) {
+                if (e.getKeyCode() == KeyEvent.VK_UP)
+                    ySpeed = -3;
+                else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+                    ySpeed = 3;
+
+            }
+            System.out.println(e.getKeyCode());
             // id가 1이면 :
             // W, S 에 따라 위 아래로 좌표 설정
             // id가 2이면 :
@@ -221,12 +207,24 @@ public class MiniPingPongGame extends JPanel implements KeyListener {
 
         void move() {
             // y방향 속도만큼 y좌표 변경
+            if (y + ySpeed > 0 && y + ySpeed < 400 - HEIGHT)
+                y += ySpeed;
         }
 
-        void draw() {
+        void draw(Graphics2D g) {
+            if (this.id == 1)
+                g.setColor(Color.blue);
+            else if (this.id == 2)
+                g.setColor(Color.red);
+            g.fillRect(this.x, this.y, this.width, this.height);
+
             // id가 1이면 파란색 지정
             // id가 2이면 빨간색 지정
             // x, y 좌표에 w, h 만큼 사각형을 채운다.
+        }
+
+        public Rectangle getBounds() {
+            return new Rectangle(x, y, WIDTH, HEIGHT);
         }
     }
 
